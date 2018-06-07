35ff64b2f42ae6feb4b47a60598ef5968535a143
Vladimir Litvinov <litvinovvo@gmail.com>
2018-06-07 18:16:01 +0500

    y is not consonant

--- task3.js
+++ task3.js
@@ -4,7 +4,7 @@ const str = args.slice(2).join(" ");
 const res = [];
 
 if(str.match(/[aeiouy]/ig))res.push(str.match(/[aeiouy]/ig).join(""))
-if(str.match(/[bcdfghjklmnpqrstvwxyz]/ig))res.push(str.match(/[bcdfghjklmnpqrstvwxyz]/ig).join(""))
+if(str.match(/[bcdfghjklmnpqrstvwxz]/ig))res.push(str.match(/[bcdfghjklmnpqrstvwxz]/ig).join(""))
 if(str.match(/[\d]/ig))res.push(str.match(/[\d]/ig).join(""))
 
 if(str.length > 0)process.stdout.write(res.join(" "));
